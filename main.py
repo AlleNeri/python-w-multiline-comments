781c780fd569bd1718f2e00bead9e5879579f631
AlleNeri <alessandro.neri21@studio.unibo.it>
2025-03-23 12:47:37 +0100

    fix(comment format): handle different comment formats

--- main.py
+++ main.py
@@ -74,21 +74,35 @@ class SnippetType(Enum):
     code = "code"
     comment = "comment"
 
+START_COMMENT = '"""'
+END_COMMENT = START_COMMENT + "\n"
+
 def split_code_every_multiline_comment(filename) -> Generator[tuple[str, SnippetType]]:
     # expecting a file content with code snippets intercalated with multiline comments, separate it and return
     # iterate over the lines
     with open(filename, "r") as f:
         line = f.readline()
         while line:
-            if line.startswith('"""'):
+            if line.startswith(START_COMMENT):
                 # multiline comment
-                multiline_comment: str = line
-                while True:
-                    line = f.readline()
-                    if not line or '"""' in line: break
-                    else: multiline_comment += line
-                # remove the two """ from the comment
-                multiline_comment = multiline_comment[4:]
+                multiline_comment: str = ""
+                # single line multiline comment :(
+                if len(line) > 4 and line.endswith(END_COMMENT):
+                    multiline_comment = line[3:-4] + "\n"
+                else:
+                    # if the comment starts in the next line than discard the first line with only `"""`
+                    # otherwise remove the `"""` from the first line and add the rest of the line to the comment
+                    if line != (START_COMMENT + '\n'):
+                        multiline_comment = line[3:]
+                    # iterate over the lines until the end of the comment
+                    while True:
+                        line = f.readline()
+                        # if the comment ends break the loop otherwise add the line to the comment
+                        if not line or line.endswith(END_COMMENT):
+                            # if there are some contents in the line add them to the comment
+                            if line and line != END_COMMENT: multiline_comment += line[:-4] + "\n"
+                            break
+                        else: multiline_comment += line
                 yield (multiline_comment, SnippetType.comment)
                 line = f.readline()
             else:
@@ -96,7 +110,7 @@ def split_code_every_multiline_comment(filename) -> Generator[tuple[str, Snippet
                 code = line
                 while True:
                     line = f.readline()
-                    if not line or '"""' in line: break
+                    if not line or START_COMMENT in line: break
                     else: code += line
                 yield (code, SnippetType.code)
 
@@ -110,7 +124,7 @@ def python_w_multiline_comments(filename: str, interactive: bool = False, fast_f
     fast_forward_handler = FastForwardHandler(fast_forward) if fast_forward else None
     for code_or_comment, type_ in split_code_every_multiline_comment(filename):
         if type_ == SnippetType.comment:
-            print(f"[bold white]{code_or_comment}[/bold white]", end="")
+            print(f"[bold white]{code_or_comment}[/bold white]")
             if interactive and fast_forward_handler:
                 fast_forward_handler.is_snippet_to_fast_forward_passed(code_or_comment)
         elif type_ == SnippetType.code:
@@ -118,6 +132,7 @@ def python_w_multiline_comments(filename: str, interactive: bool = False, fast_f
             try:
                 if not is_code_to_execute(code_or_comment): print(f"[green]Code not executed[/green]")
                 else: console.execute(code_or_comment, suppress_plots=fast_forward_handler.is_fast_forwarding() if fast_forward_handler else False)
+                print()
             except Exception as e: print(f"[bold dark_orange3]An error occurred:[/bold dark_orange3]\n[bold red]{e}[/bold red]")
         if fast_forward_handler: fast_forward_handler.increment_snippet_counter()
         if interactive:
